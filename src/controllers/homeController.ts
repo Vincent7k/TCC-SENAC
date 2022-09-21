11f5082086048f1c3492ce0f42e794a389363627
lNIcxs <nicckamorim@gmail.com>
2022-09-21 14:10:42 -0300

    Atualização

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from "express";
-import { sequelize } from "../instances/mysql";
-/*try{
-    await sequelize.authenticate()
-    console.log('Conection made sucefully')
-}catch(error){
-    console.log('We have a problem', error)
+
+
+export const home = async (req:Request, res:Response)=>{
+    
 }
-*/
-export const home = ((req:Request, res:Response)=>{
+
+/*export const home = ((req:Request, res:Response)=>{
     res.render ('pages/home')
-})
+})*/
---
+++ src/models/User.ts
@@ -0,0 +1,25 @@
+import { Model, DataTypes } from "sequelize";
+
+import {sequelize} from '../instances/mysql'
+
+export interface UserInstance  extends Model {
+    id: number
+    name:string
+    age:number
+}
+
+export const User = sequelize.define<UserInstance>('User,',{
+    id:{
+        primaryKey:true,
+        type:DataTypes.INTEGER
+    },
+    name:{
+        type:DataTypes.STRING
+    },
+    age:{
+        type:DataTypes.INTEGER,
+        defaultValue:18
+    },
+},{tableName:'Users',
+    timestamps:false
+})
